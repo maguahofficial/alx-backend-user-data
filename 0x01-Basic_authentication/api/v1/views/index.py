2b50c500cd3f7efb32f90ec0d5e9db5d186b4db6
maguah <maguahofficial@gmail.com>
2024-08-11 13:31:11 +0300

    simple-basic-API

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" The Module of Index views
+""" Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -8,28 +8,16 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
-    Function returns:
+    Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ The endpoint to testing (unauthorized) 401 error handler """
-    abort(401)
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """ The endpoint to testing (forbidden) 403 error handler """
-    abort(403)
-
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ Function GET /api/v1/stats
-    This Returns:
+    """ GET /api/v1/stats
+    Return:
       - the number of each objects
     """
     from models.user import User
